c58b48c3363e0cfe5888f6035bdc4cf8f0b1c32d
Tan Khuu <tan.khuu@karrostech.com>
2021-02-06 19:03:22 +0700

    first test create ES

--- lib/practice-cdk-stack.ts
+++ lib/practice-cdk-stack.ts
@@ -1,9 +1,26 @@
+import * as es from '@aws-cdk/aws-elasticsearch';
 import * as cdk from '@aws-cdk/core';
 
 export class PracticeCdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const esDomain = new es.Domain(this, 'Domain', {
+      version: es.ElasticsearchVersion.V7_9,
+      capacity: {
+        masterNodes: 1,
+        dataNodes: 1,
+        dataNodeInstanceType: 't3.small.elasticsearch'
+      },
+      ebs: {
+        volumeSize: 20
+      },
+      logging: {
+        slowSearchLogEnabled: true,
+        appLogEnabled: true,
+        slowIndexLogEnabled: true,
+      }
+    });
     // The code that defines your stack goes here
   }
 }
