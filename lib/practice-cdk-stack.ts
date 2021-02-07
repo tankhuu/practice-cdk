bf93dd4c4c1e85b90958c55442d542ac86153d01
Tan Khuu <tan.khuu@karrostech.com>
2021-02-07 12:29:16 +0700

    fist build bucket

--- lib/practice-cdk-stack.ts
+++ lib/practice-cdk-stack.ts
@@ -1,30 +1,17 @@
-import * as es from '@aws-cdk/aws-elasticsearch';
 import * as cdk from '@aws-cdk/core';
+import { Bucket } from '@aws-cdk/aws-s3';
+import { Duration } from '@aws-cdk/core';
 
 export class PracticeCdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const esDomain = new es.Domain(this, 'Domain', {
-      version: es.ElasticsearchVersion.V7_9,
-      capacity: {
-        dataNodes: 1,
-        dataNodeInstanceType: 't3.small.elasticsearch'
-      },
-      ebs: {
-        volumeSize: 20
-      },
-      logging: {
-        slowSearchLogEnabled: true,
-        appLogEnabled: true,
-        slowIndexLogEnabled: true,
-      }
-    });
-
-    // Outputs
-    new cdk.CfnOutput(this, 'ESDomainOutput', {
-      value: esDomain.domainName,
-      exportName: 'ESDomainName'
+    new Bucket(this, 'practiceCdkBucket', {
+      bucketName: 'practice-cdk',
+      lifecycleRules: [{
+        enabled: true,
+        expiration: Duration.days(3)
+      }]
     })
   }
 }
