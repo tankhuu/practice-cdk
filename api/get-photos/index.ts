3175211e5fe7d89d56956183e4a3395a56eb5ccc
Tan Khuu <tan.khuu@karrostech.com>
2021-02-15 23:00:48 +0700

    add api & frontend

--- api/get-photos/index.ts
+++ api/get-photos/index.ts
@@ -1,8 +1,39 @@
 import { APIGatewayProxyEventV2, Context, APIGatewayProxyStructuredResultV2 } from 'aws-lambda'
-async function getPhotos(event: APIGatewayProxyEventV2, context: Context): Promise<APIGatewayProxyStructuredResultV2> {
+import {S3} from 'aws-sdk'
+import { Bucket } from '@aws-cdk/aws-s3';
+
+const s3 = new S3()
+const bucketName = process.env.PHOTOS_BUCKET_NAME!
+
+const generateUrl = async (object: S3.Object): Promise<{fileName: string, url: string}> => {
+  const url = await s3.getSignedUrlPromise('getObject', {
+    Bucket: bucketName,
+    Key: object.Key!,
+    Expires: (24 * 60 * 60)
+  })
+
   return {
-    statusCode: 200,
-    body: 'First CDK Lambda!'
+    fileName: object.Key!,
+    url: url
+  }
+}
+
+
+async function getPhotos(event: APIGatewayProxyEventV2, context: Context): Promise<APIGatewayProxyStructuredResultV2> {
+  try {
+    const {Contents: results} = await s3.listObjects({Bucket: bucketName}).promise()
+    console.log(results)
+    const photos = await Promise.all(results?.map(r => generateUrl(r)))
+    
+    return {
+      statusCode: 200,
+      body: JSON.stringify(photos)
+    }
+  } catch(err) {
+    return {
+      statusCode: 500,
+      body: err.message
+    }
   }
 }
 
--- lib/practice-cdk-stack.ts
+++ lib/practice-cdk-stack.ts
@@ -4,6 +4,10 @@ import { Bucket } from '@aws-cdk/aws-s3';
 import { CfnOutput, Duration } from '@aws-cdk/core';
 import * as path from 'path'
 import {Runtime} from '@aws-cdk/aws-lambda'
+import { BucketDeployment, Source } from '@aws-cdk/aws-s3-deployment';
+import { PolicyStatement } from '@aws-cdk/aws-iam';
+import { HttpApi, HttpMethod } from '@aws-cdk/aws-apigatewayv2';
+import { LambdaProxyIntegration } from '@aws-cdk/aws-apigatewayv2-integrations';
 
 export class PracticeCdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -13,18 +17,80 @@ export class PracticeCdkStack extends cdk.Stack {
       bucketName: 'practice-cdk',
       lifecycleRules: [{
         enabled: true,
+        expiration: Duration.days(3)
       }]
+    })
+
+    new BucketDeployment(this, 'AppPhotos', {
+      sources: [
+        Source.asset(path.join(__dirname, '..', 'photos'))
+      ],
+      destinationBucket: bucket
+    })
+
+    const frontendBucket = new Bucket(this, 'ReactAppWeb', {
+      websiteIndexDocument: 'index.html',
+      publicReadAccess: true
+    })
+
+    new BucketDeployment(this, 'ReactAppWebDeploy', {
+      sources: [
+        Source.asset(path.join(__dirname, '..', 'frontend', 'build'))
+      ],
+      destinationBucket: frontendBucket
+    })
+
+    const bucketContainersPermissions = new PolicyStatement()
+    bucketContainersPermissions.addResources(bucket.bucketArn)
+    bucketContainersPermissions.addActions('s3:ListBucket')
+
+    const bucketPermissions = new PolicyStatement()
+    bucketPermissions.addResources(`${bucket.bucketArn}/*`)
+    bucketPermissions.addActions('s3:GetObject', 's3:PutObject')
+
+    const getPhotosFunc = new lambda.NodejsFunction(this, 'getPhotosLambda', {
+      runtime: Runtime.NODEJS_12_X,
+      entry: path.join(__dirname, '..', 'api', 'get-photos', 'index.ts'),
+      handler: 'getPhotos',
+      environment: {
+        PHOTOS_BUCKET_NAME: bucket.bucketName
+      }
+    })
+
+    getPhotosFunc.addToRolePolicy(bucketPermissions)
+    getPhotosFunc.addToRolePolicy(bucketContainersPermissions)
+
+    const httpApi = new HttpApi(this, 'getPhotosApi', {
+      apiName: 'photos',
+      corsPreflight: {
+        allowOrigins: ['*'],
+        allowMethods: [HttpMethod.GET],
+      },
+      createDefaultStage: true
+    })
+
+    const lambdaIntegration = new LambdaProxyIntegration({
+      handler: getPhotosFunc
+    })
+
+    httpApi.addRoutes({
+      path: '/photos',
+      methods: [HttpMethod.GET],
+      integration: lambdaIntegration
     })
 
     new CfnOutput(this, 'PracticeCDKBucketNameExport', {
       exportName: 'PracticeCDKBucketName',
       value: bucket.bucketName
     })
+    new CfnOutput(this, 'FrontendBucketNameExport', {
+      exportName: 'FrontendBucketName',
+      value: frontendBucket.bucketName
+    })
 
-    const getPhotosFunc = new lambda.NodejsFunction(this, 'getPhotosLambda', {
-      runtime: Runtime.NODEJS_12_X,
-      entry: path.join(__dirname, '..', 'api', 'get-photos', 'index.ts'),
-      handler: 'getPhotos',
+    new CfnOutput(this, 'photosApi', {
+      exportName: 'PhotosApiEndpoint',
+      value: httpApi.url!
     })
   }
 }
